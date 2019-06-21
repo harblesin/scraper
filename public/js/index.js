7633983e281cf10b16397847da791d5102f01fbf
h f <waltermelonson@gmail.com>
2019-06-20 21:02:46 -0700

    note model added, note form now appears upon clicking on an article, styled using bootstrap, need to add save to db functionality as well as connection to article model

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,16 @@ $("#clear").click(function() {
   });
   //location.reload();
 });
+
+$("h3").click(function(){
+    $(".notes").empty();
+    $(".notes").append($(`<form><div class='form-group' style='margin-top:100px;width:200px'>
+    Name<br><input class='form-control'></input></div>
+    Note<br><div class='form-group'><textarea class='form-control' style='width:200px;height:600px'></textarea></div></form>
+    <button class='btn btn-info save'>Save</button>`))
+    // let titleBox = $("<input>").attr("type","text");
+    // let noteBox = $("<textarea>").attr("style", "width:200px;height:600px");
+    // titleBox.wrap($("<label>").text("Name"));
+    // noteBox.wrap($("<label>").text("Note"));
+    // $("input").wrapAll("<form></form>").appendTo($(".notes"));
+})
