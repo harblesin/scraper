4f7f038636f5fb7ec36c16cd95e03a313e73c8e6
h f <waltermelonson@gmail.com>
2019-06-20 22:36:21 -0700

    note model is set up still, reconfigured how the buttons work, populate button is no longer needed, scrape button now scrapes and reloads page with results from database on one click

--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,55 @@
-$("#scrape").click(function() {
-  console.log("click");
-  $.getJSON("/scrape", function(data) {
-    console.log(data);
-  });
-});
-$("#pop").click(function() {
-  location.reload();
-
-  // $.ajax({
-  //         method: "GET",
-  //         url: "/populate/"
-  //     })
-  //     .then(function (data) {
-
-  //         }
-
-  //     )
+$("#scrape").click(function () {
+    console.log("click");
+    $.get("/scrape", function (data) {
+        console.log(data);
+       
+    }).then(function(){
+        location.reload();
+    });
+     
 });
 
-$("#clear").click(function() {
-  $(".jumbotron").empty();
-  $.ajax({
-    method: "DELETE",
-    url: "/delete/"
-  }).then(function(data) {
-    res.json(data);
-  });
-  //location.reload();
+$("#pop").click(function () {
+    location.reload();
+
+    // $.ajax({
+    //         method: "GET",
+    //         url: "/populate/"
+    //     })
+    //     .then(function (data) {
+
+    //         }
+
+    //     )
 });
 
-$("h3").click(function(){
+$("#clear").click(function () {
+    //$(".jumbotron").empty();
+    $.ajax({
+        method: "DELETE",
+        url: "/delete/"
+    }).then(function (data) {
+        res.json(data);
+        
+    });
+    location.reload();
+});
+
+//I know this is ugly and monstrous but I found amusement in the thought of it after writing it trying
+//to circumvent some jQuery syntax and with the thought that i've seen no one do this ever,
+//assumedly because it's ugly and not good practice, I thought it was funny and left it.
+$("h3").click(function () {
     $(".notes").empty();
-    $(".notes").append($(`<form><div class='form-group' style='margin-top:100px;width:200px'>
+    $(".notes").append($(`<form><div class='form-group'
+     style='margin-top:100px;width:200px'>
     Name<br><input class='form-control'></input></div>
-    Note<br><div class='form-group'><textarea class='form-control' style='width:200px;height:600px'></textarea></div></form>
+    Note<br><div class='form-group'><textarea class='form-control'
+     style='width:200px;height:600px'></textarea></div></form>
     <button class='btn btn-info save'>Save</button>`))
     // let titleBox = $("<input>").attr("type","text");
     // let noteBox = $("<textarea>").attr("style", "width:200px;height:600px");
     // titleBox.wrap($("<label>").text("Name"));
     // noteBox.wrap($("<label>").text("Note"));
     // $("input").wrapAll("<form></form>").appendTo($(".notes"));
+
 })
